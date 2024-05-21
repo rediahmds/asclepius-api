aacc034640fb65e34ee7ffbf9f3b664dc36a8c9c
rediahmds_ubuntu <redi.ngoding@gmail.com>
2024-05-21 15:50:30 +0700

    fix: collection naming

--- src/services/firestoreInteract.js
+++ src/services/firestoreInteract.js
@@ -3,12 +3,12 @@ const { Firestore } = require("@google-cloud/firestore");
 const db = new Firestore();
 
 function storeData(id, data) {
-  const predictCollection = db.collection("prediction");
+  const predictCollection = db.collection("predictions");
   return predictCollection.doc(id).set(data);
 }
 
 async function getHistories() {
-  const snapshot = await db.collection("prediction").get();
+  const snapshot = await db.collection("predictions").get();
   const histories = [];
   snapshot.forEach((doc) => histories.push({ id: doc.id, history: doc.data() }));
 
