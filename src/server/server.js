99b60400ca9db3c2bcb69f364059bbcb187ca38b
rediahmds_ubuntu <redi.ngoding@gmail.com>
2024-05-21 08:25:56 +0700

    create preResponse handling

--- src/server/server.js
+++ src/server/server.js
@@ -1,4 +1,6 @@
 const Hapi = require("@hapi/hapi");
+const routes = require("./routes");
+const InputError = require("../exceptions/InputError");
 
 (async () => {
   const server = Hapi.server({
@@ -10,6 +12,44 @@ const Hapi = require("@hapi/hapi");
       },
     },
   });
+
+  server.route(routes);
+
+  server.ext("onPreResponse", function (request, h) {
+    const response = request.response;
+
+    if (response.isBoom && response.statusCode === 413) {
+      const newResponse = h
+        .response({
+          status: "fail",
+          message:
+            "Payload content length greater than maximum allowed: 1000000",
+        })
+        .code(413);
+
+      return newResponse;
+    }
+
+    if (response instanceof InputError) {
+      // InputError akan berasal dari file InputError.js
+      const newResponse = h.response({
+        status: "fail",
+        message: `${response.message} Silakan gunakan foto lain.`,
+      });
+      newResponse.code(response.statusCode);
+      return newResponse;
+    }
+    if (response.isBoom) {
+      const newResponse = h.response({
+        status: "fail",
+        message: response.message,
+      });
+      newResponse.code(response.statusCode);
+      return newResponse;
+    }
+    return h.continue;
+  });
+
   server.start();
   console.log(`Server is running on ${server.info.uri}`);
 })();
